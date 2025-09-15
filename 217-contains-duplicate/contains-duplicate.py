cc8e304c80ae266db464328974a999e56ef76d76
a-chiha <ahmadchiha01@gmail.com>
2025-09-15 20:11:58 +0200

    Time: 43 ms (12.22%) | Memory: 34.9 MB (9.73%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,12 +1,16 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        myset = set()
+        map1 = {}
         for i in nums:
-            if i not in myset:
-                myset.add(i)
+            if i not in map1:
+                map1[i] = 1
             else:
+                map1[i] +=1
+        for key,value in map1.items():
+            if value > 1:
                 return True
         return False
+        
 
 
 
