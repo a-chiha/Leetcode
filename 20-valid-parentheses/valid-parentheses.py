b2494338b70af05eb3976361d5b12e28decaff42
a-chiha <ahmadchiha01@gmail.com>
2025-10-16 16:37:52 +0200

    Time: 2 ms (39.70%) | Memory: 17.8 MB (85.61%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -6,7 +6,7 @@ class Solution:
         for i in s:
             if i in map1.values():
                 stack.append(i)
-            elif len(stack) > 0 and map1.get(i,0) == stack[-1]:
+            elif stack and map1.get(i,0) == stack[-1]:
                 stack.pop()
             else:
                 return False
